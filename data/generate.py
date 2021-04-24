af7c3993d4e4fec2c445b5f1128b8b4efb35f211
Ted Wong <tedwong@smallchess.com>
2021-04-24 10:16:26 +1000

    ...

--- data/generate.py
+++ data/generate.py
@@ -11,7 +11,17 @@ def read(name, file):
             w.write("{ 'id':" + str(toks[0]) + ", 'fen':'" + fen + "' },\n")
         w.write("];\n\n")
 
-read("Courses['beginner']", "data/beginner.tsv")
+read("Courses['beginner']",     "data/beginner.tsv")
+read("Courses['misc01']",       "data/misc01.tsv")
+read("Courses['misc02']",       "data/misc02.tsv")
+read("Courses['misc03']",       "data/misc03.tsv")
+read("Courses['misc04']",       "data/misc04.tsv")
+read("Courses['misc05']",       "data/misc05.tsv")
+read("Courses['queens']",       "data/queens.tsv")
+read("Courses['bishops']",      "data/bishops.tsv")
+read("Courses['rooks']",        "data/rooks.tsv")
+read("Courses['knights']",      "data/knights.tsv")
+read("Courses['pawns']",        "data/pawns.tsv")
 read("Courses['intermediate']", "data/intermediate.tsv")
 
 w.close()
